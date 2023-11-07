b10105399f234cf1316842522c8d04fc8e29fc88
DJousto <vincent.digiusto@gmail.com>
2023-11-07 17:09:22 +0100

    adding price and packaging functions

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='graphee-functions',
+    version='0.0.1',
+    py_modules=['graphee-functions'],
+    install_requires=[
+        'requests',
+    ],
+)
